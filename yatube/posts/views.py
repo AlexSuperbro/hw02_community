519f55f539126b05f546a97232f215b95c0c7a86
Александр Нестерихин <NesterAE@mail.ru>
2022-05-05 21:06:20 +0300

    Pytest compleated. First commit

---
+++ yatube/posts/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post, Group
+
+# Create your views here.
+def index(request):
+    text = 'Последние обновления на сайте'
+    template = 'posts/index.html'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+        'text': text
+    }
+    return render(request, template, context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
